9eccf8ae5bc32bf5bec63b2fb016f1eb2e787ce8
GeoffroyK <geoffroy.keime@gmail.com>
2025-02-10 06:09:52 +0100

    add constant file for paths

---
+++ deep_models/constant_paths.py
@@ -0,0 +1,7 @@
+hot_pixels_locations = {
+    "sunset1": "/home/geoffroyk/brisbane_hot_pixels/dvs_vpr_2020-04-21-17-03-03_hot_pixels.txt",
+    "sunset2": "/home/geoffroyk/brisbane_hot_pixels/dvs_vpr_2020-04-22-17-24-21_hot_pixels.txt",
+    "sunrise": "/home/geoffroyk/brisbane_hot_pixels/dvs_vpr_2020-04-29-06-20-23_hot_pixels.txt",
+    "daytime": "/home/geoffroyk/brisbane_hot_pixels/dvs_vpr_2020-04-24-15-12-03_hot_pixels.txt",
+    "morning": "/home/geoffroyk/brisbane_hot_pixels/dvs_vpr_2020-04-28-09-14-11_hot_pixels.txt",
+}
